4cfbe254a231409c9460ab5aa53bec890bc6ba8a
Sravya-V <36848376+Sravya-V@users.noreply.github.com>
2024-05-08 17:45:26 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+const port = 3000;
+
+const app = express();
+
+// Allow requests only from your Netlify app URL
+const corsOptions = {
+  origin: 'https://sweet-sorbet-5077bc.netlify.app',
+  credentials: true, // Allow sending cookies
+};
+
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/", routes);
+
+app.use(function(req, res, next) {
+  res.status(404).json({ message: "No such route exists" });
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+mongoose.connect("mongodb+srv://svemuri:svemuri123@test.q6pcyki.mongodb.net/test", {
+  useNewUrlParser: true,
+})
+  .then(() => {
+    console.log("connected to database");
+
+    app.listen(port, () => {
+      console.log(`API listening to http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+  });
